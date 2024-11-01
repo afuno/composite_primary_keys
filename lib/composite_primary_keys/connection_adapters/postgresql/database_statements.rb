6e777ea5c5531f5239f6e88645e53873e6157687
Anton Sokolov <anton.s@codexoptimus.pro>
2024-11-01 23:05:40 +0700

    Refactor SQL insert method to support RETURNING clause
    Updated the `sql_for_insert` method to handle the `RETURNING` clause when supported by PostgreSQL. Enhanced the method to ensure proper construction of the returning columns statement, resulting in more efficient and accurate query execution.

--- lib/composite_primary_keys/connection_adapters/postgresql/database_statements.rb
+++ lib/composite_primary_keys/connection_adapters/postgresql/database_statements.rb
@@ -2,23 +2,21 @@ module ActiveRecord
   module ConnectionAdapters
     module PostgreSQL
       module DatabaseStatements
-        def sql_for_insert(sql, pk, binds) # :nodoc:
-          if pk.nil?
-            # Extract the table from the insert sql. Yuck.
-            table_ref = extract_table_ref_from_insert_sql(sql)
-            pk = primary_key(table_ref) if table_ref
+        def sql_for_insert(sql, pk, binds, returning) # :nodoc:
+          if supports_insert_returning?
+            if pk.nil?
+              # Extract the table from the insert sql. Yuck.
+              table_ref = extract_table_ref_from_insert_sql(sql)
+              pk = primary_key(table_ref) if table_ref
+            end
+
+            returning_columns = returning || Array(pk)
+
+            returning_columns_statement = returning_columns.map { |c| quote_column_name(c) }.join(", ")
+            sql = "#{sql} RETURNING #{returning_columns_statement}" if returning_columns.any?
           end
 
-          # CPK
-          # if pk = suppress_composite_primary_key(pk)
-          #  sql = "#{sql} RETURNING #{quote_column_name(pk)}"
-          #end
-          # NOTE pk can be false.
-          if pk
-            sql = "#{sql} RETURNING #{quote_column_names(pk)}"
-          end
-
-          super
+          [sql, binds]
         end
       end
     end
